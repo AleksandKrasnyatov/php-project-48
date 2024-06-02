ee5050a7d0c799eedf4274ad781aa3cc5a2904dd
AlekasndKrasnyatov <ssokoloss1@gmail.com>
2024-06-02 12:38:23 +0300

    refactor and add 3 step of project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AleksandKrasnyatov/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AleksandKrasnyatov/php-project-48/actions)
+
+# Asciinema recording of comparing 2 files:
+https://asciinema.org/a/aEjg9Y9FT1OBbHFCU4EUfMQhy
---
+++ src/Diff.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Package\Diff;
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $path1 = realpath($pathToFile1);
+    $path2 = realpath($pathToFile2);
+
+    if ($path1 === false || $path2 === false) {
+        return "Error! Can't find the file(-s)!";
+    }
+
+    if (!file_exists($path1) || !file_exists($path2)) {
+        return "Error! File(-s) doesn't exists!";
+    }
+
+    $json1 = file_get_contents($path1);
+    $json2 = file_get_contents($path2);
+
+    $content1 = json_decode($json1, true);
+    $content2 = json_decode($json2, true);
+
+    $differences = compareArrays($content1, $content2);
+    return render($differences);
+}
+
+function compareArrays(array $arr1, array $arr2): array
+{
+    $result = [];
+    $allUniqueKeys = array_keys(array_merge($arr1, $arr2)); 
+    sort($allUniqueKeys);
+    foreach ($allUniqueKeys as $key) {
+        if (!array_key_exists($key, $arr1)) {
+            $result["+ {$key}"] = $arr2[$key];
+        } elseif (!array_key_exists($key, $arr2)) {
+            $result["- {$key}"] = $arr1[$key];
+        } elseif ($arr1[$key] === $arr2[$key]) {
+            $result[$key] = $arr1[$key];
+        } else {
+            $result["- {$key}"] = $arr1[$key];
+            $result["+ {$key}"] = $arr2[$key];
+        }
+    }
+    return $result;
+}
+
+function render(array $items): string
+{
+    $resultArray = [];
+    foreach ($items as $key => $item) {
+        if (!is_string($item)) {
+            $item = var_export($item, true);
+        }
+        $resultArray[] = "  {$key}: {$item}";
+    }
+    $resultStr = implode("\n", $resultArray);
+    return "{" . "\n" . $resultStr . "\n" . "}" . "\n";
+}
+
+function compareArraysYoing(array $arr1, array $arr2): array
+{
+    $result = [];
+    foreach ($arr1 as $key1 => $item1) {
+        if (array_key_exists($key1, $arr2)) {
+            if ($item1 != $arr2[$key1]) {
+                $result[$key1 . '1'] = '- ' . $key1 . ": " . var_export($item1, true); 
+                $result[$key1 . '2'] = '+ ' . $key1 . ": " . var_export($arr2[$key1], true); 
+            } else {
+                $result[$key1] = $key1 . ": " . var_export($item1, true);
+            }
+            unset($arr2[$key1]);
+        } else {
+            $result[$key1] = '- ' . $key1 . ": " . var_export($item1, true); 
+        }
+    }
+    foreach ($arr2 as $key2 => $item2) {
+        $result[$key2] = '+ ' . $key2 . ": " . var_export($item2, true);
+    }
+    return $result;
+}
